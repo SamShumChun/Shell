213a17a41348b2a6ffe79d75ad7065526a1930d8
Sam Shum <38761558+SamShumChun@users.noreply.github.com>
2025-07-12 06:26:40 +0800

    Update AutoDD.sh

--- AutoDD.sh
+++ AutoDD.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 clear
-echo "This script is deprecated, use https://git.io/AutoReinstall.sh instead."
+echo "This script is deprecated, use https://raw.githubusercontent.com/SamShumChun/Shell/AutoReinstall.sh instead."
